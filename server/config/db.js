3ef4bb4bd72b91d1816b24525180a23eafae6898
SaqibSiddiqi <lookfright@gmail.com>
2022-03-05 21:56:27 +0400

    Heroku set

--- server/config/db.js
+++ server/config/db.js
@@ -6,5 +6,7 @@
  */
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://SaqibSiddiqi:siddiqi@midterm.u4hou.mongodb.net/Midterm?retryWrites=true&w=majority"
+  //"URI": "mongodb+srv://SaqibSiddiqi:siddiqi@midterm.u4hou.mongodb.net/Midterm?retryWrites=true&w=majority"
+  //using heroku
+  "URI":  process.env.MONGODB_URI
 };
